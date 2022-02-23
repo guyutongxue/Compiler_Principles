1171b78369122390120578b646cead98d26c9527
Guyutongxue <guyutongxue@163.com>
2022-02-23 21:49:58 +0800

    lv1.2

---
+++ build.rs
@@ -0,0 +1,4 @@
+// https://lalrpop.github.io/lalrpop/tutorial/001_adding_lalrpop.html
+fn main() {
+  lalrpop::process_root().unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use lalrpop_util::lalrpop_mod;
+use std::env::args;
+use std::error::Error;
+use std::fs::read_to_string;
+
+lalrpop_mod!(sysy);
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let mut args = args();
+  args.next();
+  let mode = args.next().expect("missing mode");
+  let input = args.next().expect("missing input");
+  args.next();
+  let output = args.next().expect("missing output");
+
+  let input = read_to_string(input)?;
+
+  let ast = sysy::CompUnitParser::new().parse(&input).unwrap();
+
+  println!("{}", ast);
+  Ok(())
 }
